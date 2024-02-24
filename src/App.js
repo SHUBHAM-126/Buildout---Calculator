954a38a3b98c008ee38af984e5fdd2613ef6d02c
SHUBHAM-126 <shubhammhatre126@gmail.com>
2024-02-24 20:11:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [ inField, setInField ] = useState('')
+  const [ ans, setAns ] = useState(null)
+
+  const handleClick = (val) => {
+    
+    if(val == "C"){
+      setInField('')
+      setAns(null)
+    }
+    else if(val == "="){
+      setAns(eval(inField))
+    }
+    else{
+      setInField(prev => prev+val)
+    } 
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-wrapper">
+      <h1>React Calculator</h1>
+      <input type="text" value={inField} onChange={(e)=> setInField(e.target.value)} /> 
+
+      {ans != null && <p className='anwser'>{ans}</p>}
+
+      <div className="button-container">
+        <button onClick={() => handleClick(7)}>7</button>
+        <button onClick={() => handleClick(8)}>8</button>
+        <button onClick={() => handleClick(9)}>9</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick(4)}>4</button>
+        <button onClick={() => handleClick(5)}>5</button>
+        <button onClick={() => handleClick(6)}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick(1)}>1</button>
+        <button onClick={() => handleClick(2)}>2</button>
+        <button onClick={() => handleClick(3)}>3</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('C')}>C</button>
+        <button onClick={() => handleClick(0)}>0</button>
+        <button onClick={() => handleClick('=')}>=</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
     </div>
   );
 }
